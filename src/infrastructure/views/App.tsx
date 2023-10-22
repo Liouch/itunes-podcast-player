36daf8c9b1843ade41df5f35d2eb5cfae03b5ba7
Liou <liou.wang.it@gmail.com>
2023-10-22 16:49:53 +0200

    [Project Setup]: Add StyledEngineProvider to avoid Tailwind CSS styles getting overriden by MUI

--- src/infrastructure/views/App.tsx
+++ src/infrastructure/views/App.tsx
@@ -1,15 +1,19 @@
+import { StyledEngineProvider } from '@mui/material/styles';
 import './App.css';
 import PodCastPlayer from './components/Podcast';
 import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <main className='min-w-screen min-h-screen flex flex-col items-center'>
-      <div className='w-4/5 md:w-2/4 flex flex-col items-center mt-6'>
-        <Outlet />
-      </div>
-      <PodCastPlayer />
-    </main>
+    // Add StyledEngineProvider to avoid MUI styles to override TailWindCSS
+    <StyledEngineProvider injectFirst>
+      <main className='min-w-screen min-h-screen flex flex-col items-center'>
+        <div className='w-4/5 md:w-2/4 flex flex-col items-center mt-6'>
+          <Outlet />
+        </div>
+        <PodCastPlayer />
+      </main>
+    </StyledEngineProvider>
   );
 }
 
